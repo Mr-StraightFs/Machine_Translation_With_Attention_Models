3305ddb6f96dc88a2d1b44e43ead54950635a552
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-15 20:51:17 +0100

    Understandng and Processing the Dta

--- Neural_Machine_Trans_Attention.py
+++ Neural_Machine_Trans_Attention.py
@@ -16,3 +16,20 @@ import matplotlib.pyplot as plt
 # Loading the Dataset
 m = 10000
 dataset, human_vocab, machine_vocab, inv_machine_vocab = load_dataset(m)
+
+# Let's preprocess the data and map the raw text data into the index values.
+# We will set Tx=30
+# We assume Tx is the maximum length of the human readable date.
+# If we get a longer input, we would have to truncate it.
+# We will set Ty=10
+# "YYYY-MM-DD" is 10 characters long
+Tx = 30
+Ty = 10
+X, Y, Xoh, Yoh = preprocess_data(dataset, human_vocab, machine_vocab, Tx, Ty)
+print("X.shape:", X.shape)
+print("Y.shape:", Y.shape)
+print("Xoh.shape:", Xoh.shape)
+print("Yoh.shape:", Yoh.shape)
+
+
+
